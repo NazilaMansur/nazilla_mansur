aa1d89cd589efcee246d89af0b4d590c1c6a94d3
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-09 20:55:51 +0800

    hari1.java
    tipedata

---
+++ hari1.java
@@ -0,0 +1,26 @@
+package hari1;
+
+public class Hari1 {
+
+    public static void main(String[] args) {
+        String nama = "NazilaMansur";
+     String alamat = "Sendana";
+     String nim = "D0221035";
+     char kelas = 'C';
+     String prodi = "informatika";
+     long angkatan = 2021;
+     double tinggi_badan = 156;
+     float berat_badan = 47f;
+     boolean belum_menikah = true;
+     System.out.println("Nama :"+nama);
+     System.out.println("Alamat :"+alamat);
+     System.out.println("Nim :"+nim);
+     System.out.println("Kelas :"+kelas);
+     System.out.println("Prodi :"+prodi);
+     System.out.println("Angkatan :"+angkatan);
+     System.out.println("Tinggi badan :"+tinggi_badan);
+     System.out.println("Berat badan :"+berat_badan);
+     System.out.println("Belum menikah :"+belum_menikah);
+    }
+    
+}
