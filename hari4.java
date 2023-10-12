759b3cbbcc8c4469363f20279507d8b63bdfbac2
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-12 21:46:53 +0800

    hari4.java

--- hari4.java
+++ hari4.java
@@ -1,40 +1,31 @@
 package days.of.coding;
 import java.util.Scanner;
+
 public class Day4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String nama, alamat, pekerjaan, agama;
-        int umur;
-        double tinggiBadan, beratBadan;
+        String makanan, minuman, hobi;
+        int usia;
+       
 
-        System.out.print("Masukkan Umur           : ");
-        umur = scan.nextInt();
+        System.out.print("Masukkan Makanan Favorit Anda: ");
+        makanan = scan.nextLine();
 
-        System.out.print("Masukkan Pekerjaan      : ");
-        scan.nextLine(); 
-        pekerjaan = scan.nextLine();
+        System.out.print("Masukkan Minuman Favorit Anda: ");
+        minuman = scan.nextLine();
 
-        System.out.print("Masukkan Nama           : ");
-        nama = scan.nextLine();
+        System.out.print("Masukkan Hobi Anda: ");
+        hobi = scan.nextLine();
 
-        System.out.print("Masukkan Alamat         : ");
-        alamat = scan.nextLine();
+        System.out.print("Berapa Usia Anda: ");
+        usia = scan.nextInt();
 
-        System.out.print("Masukkan Tinggi Badan   : ");
-        tinggiBadan = scan.nextDouble();
-
-        System.out.print("Masukkan Agama          : ");
-        scan.nextLine(); 
-        agama = scan.nextLine();
-
-        System.out.print("Masukkan Berat Badan    : ");
-        beratBadan = scan.nextDouble();
-
+        
         System.out.println("===============================================");
-        System.out.println("Umur Saya " + umur + " Tahun");
-        System.out.println("Pekerjaan sebagai " + pekerjaan + " Agama Saya " + agama);
-        System.out.println("Nama Saya " + nama + " Lahir di " + alamat);
-        System.out.println("Tinggi Badan Saya " + tinggiBadan + " CM");
-        System.out.println("BeratBadan Saya " + beratBadan + " KG");
+        System.out.println("Makanan Favorit Anda: " + makanan);
+        System.out.println("Minuman Favorit Anda: " + minuman);
+        System.out.println("Hobi Anda: " + hobi);
+        System.out.println("Usia Anda: " + usia + " tahun");
+        
     }
 }
