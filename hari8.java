418a5a61202c8803545fa52df476a7fa49a0c4fb
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-16 22:53:18 +0800

    hari8.java

---
+++ hari8.java
@@ -0,0 +1,16 @@
+package Days_of_coding
+import java.util.Scanner;
+
+public class Day8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int angka = 10; // Ganti 10 dengan angka yang ingin Anda uji.
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " adalah genap.");
+        } else {
+            System.out.println("Angka " + angka + " adalah ganjil.");
+        }
+    }
+}
