8de6fbd3375bd1d00fff42bb107cf9582e87630f
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-21 00:04:19 +0800

    hari12.java

---
+++ hari12.java
@@ -0,0 +1,9 @@
+package days.of.coding;
+
+public class day12 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
