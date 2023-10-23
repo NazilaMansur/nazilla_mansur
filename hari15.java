7720a941d43c3bc344e62fe6fe4b91837bf811b9
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-23 22:31:27 +0800

    hari15.java

---
+++ hari15.java
@@ -0,0 +1,23 @@
+package days.of.coding;
+
+import java.util.Scanner;
+
+public class ContohTipeData {
+    public static void main(String[] args) {
+        // Mendeklarasikan dan menginisialisasi variabel dengan tipe data int
+        int angkaPertama = 5;
+        int angkaKedua = 10;
+
+        // Melakukan operasi matematika
+        int hasilPenjumlahan = angkaPertama + angkaKedua;
+        int hasilPengurangan = angkaPertama - angkaKedua;
+        int hasilPerkalian = angkaPertama * angkaKedua;
+        int hasilPembagian = angkaPertama / angkaKedua;
+
+        // Menampilkan hasil operasi
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
