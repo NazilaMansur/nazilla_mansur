56768dc8ecb9e9b16c7d723213938c57fd93affb
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-24 23:08:14 +0800

    hari16.java

---
+++ hari16.java
@@ -0,0 +1,21 @@
+package days.of.coding;
+
+import java.util.Scanner;
+
+
+public class AritmatikaSederhana {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 3;
+
+        int penambahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+        int perkalian = angka1 * angka2;
+        double pembagian = (double) angka1 / angka2;
+
+        System.out.println("Penambahan: " + penambahan);
+        System.out.println("Pengurangan: " + pengurangan);
+        System.out.println("Perkalian: " + perkalian);
+        System.out.println("Pembagian: " + pembagian);
+    }
+}
