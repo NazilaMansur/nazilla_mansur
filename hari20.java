d17104f172ee49079fcfa85fdd2cdab2fc19a1da
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-28 23:45:42 +0800

    hari20.java

---
+++ hari20.java
@@ -0,0 +1,20 @@
+package days.of.coding;
+
+import java.util.Scanner;      
+
+public class SimpleArithmetic3 {
+    public static void main(String[] args) {
+        int angka1 = 8;
+        int angka2 = 4;
+
+        int penjumlahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+        int perkalian = angka1 * angka2;
+        int pembagian = angka1 / angka2;
+
+        System.out.println("Penjumlahan: " + penjumlahan);
+        System.out.println("Pengurangan: " + pengurangan);
+        System.out.println("Perkalian: " + perkalian);
+        System.out.println("Pembagian: " + pembagian);
+    }
+}
