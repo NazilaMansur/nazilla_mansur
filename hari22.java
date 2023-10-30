f6e8569fe37e81e712a1ec8c4d0282f63503e1f9
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-10-30 23:11:12 +0800

    hari22.java
    perulangan For

---
+++ hari22.java
@@ -0,0 +1,9 @@
+package days.of.coding;
+
+public class PerulanganFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
