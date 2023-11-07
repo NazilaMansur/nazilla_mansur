8e3e43078e9be026a18e04dd34fa17616eec374a
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-11-07 22:41:48 +0800

    hari30.java
    percabangan

---
+++ hari30.java
@@ -0,0 +1,15 @@
+package days.of.coding;
+
+public class days30Java {
+    public static void main(String[] args) {
+        // dua variabel dengan nilai 
+        int angka1 = 5;
+        int angka2 = 3;
+
+        // menjumlahkan dua angka
+        int hasilPenjumlahan = angka1 + angka2;
+
+        // menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan dari " + angka1 + " dan " + angka2 + " adalah " + hasilPenjumlahan);
+    }
+}
