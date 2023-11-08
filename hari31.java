b9bdb423382b0bf7911f41109e8f40ca6c2ac3f2
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-11-08 22:48:20 +0800

    hari31.java
    aritmatika sederhana

---
+++ hari31.java
@@ -0,0 +1,10 @@
+package days.of.coding;
+
+public class AritmatikaSederhana {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 3;
+        int hasil = angka1 + angka2;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
