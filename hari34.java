022497cd93f646dbfb8b93f36201270e2ba68d33
NazilaMansur <100031140+NazilaMansur@users.noreply.github.com>
2023-11-11 21:17:15 +0800

    hari34.java
    menggunakan for

---
+++ hari34.java
@@ -0,0 +1,10 @@
+package days.of.coding;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // perulangan for
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Variabel ke-" + i);
+        }
+    }
+}
